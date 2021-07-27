7e4b51124125653c0e8b8f6a057de0c6719df0fe
Tanner-Wills <tannerwills1@gmail.com>
2021-07-27 13:13:43 -0400

    Added linkedList class

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,5 +1,25 @@
 public class LinkedList <T> implements List<T>{
 
+    //Instance Variables
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    //Constructors
+    public LinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    //Getters
+    public Node<T> getHead(){
+        return this.head;
+    }
+    public Node<T> getTail(){
+        return this.tail;
+    }
+
+    //Linked List Methods
 
     @Override
     public void addAtIndex(T data, int index) {
